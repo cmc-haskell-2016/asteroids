d1075a0676feabb6e553d2090731f690c88d33cb
Татьяна <tatiana@t.const>
2016-03-21 11:13:12 +0300

    Main.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Lib
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game
 --import Graphics.Gloss.Interface.IO.Game
 
 type Radius = Float
@@ -33,8 +34,8 @@ background = makeColorI 75 75 186 0
 shipColor :: Color
 shipColor = black
 
-drawingShip :: Position -> Degree -> Picture
-drawingShip (x, y) phi = pictures[
+drawingShip :: GameState -> Picture
+drawingShip game = pictures[
 	translate x y $
 	color shipColor $
 	rotate phi $
@@ -43,6 +44,11 @@ drawingShip (x, y) phi = pictures[
 	color shipColor $
 	rotate phi $
 	polygon [(-10, -5), (0, 0), (0, 20)]]
+	where
+		(x, y) = shipLoc game
+		(xv, yv) = shipVel game
+		phi = shipAng game
+		--phi = (atan2 xv yv) * 180 / pi
 
 --drawing :: Picture
 --drawing = pictures[
@@ -53,18 +59,18 @@ drawing = render initialState
 
 data GameState = Game {
 	shipLoc :: Position,
-	shipAng :: Degree,
+	shipAng :: Float,
 	shipVel :: (Float, Float)
 } deriving Show
 
 render :: GameState -> Picture
-render game = pictures [(drawingShip (shipLoc game) (shipAng game))]
+render game = pictures [(drawingShip game)]
 
 initialState :: GameState
 initialState = Game {
 	shipLoc = (0, -100),
 	shipAng = 0,
-	shipVel = (0, 50)
+	shipVel = (0, 0)
 }
 
 deathState :: GameState
@@ -75,7 +81,9 @@ deathState = Game {
 }
 
 moveShip :: Float -> GameState -> GameState
-moveShip sec game = game {shipLoc = (x1, y1)}
+moveShip sec game = if vx == 0 && vy == 0 
+					then game 
+					else game {shipLoc = (x1, y1), shipAng = (atan2 vx vy) * 180 / pi}
 	where
     (x, y) = shipLoc game
     (vx, vy) = shipVel game
@@ -85,8 +93,8 @@ moveShip sec game = game {shipLoc = (x1, y1)}
 frame :: Float -> Picture
 frame seconds = render $ moveShip seconds initialState
 
-update :: ViewPort -> Float -> GameState -> GameState
-update _ seconds = wallDeath . moveShip seconds
+update :: Float -> GameState -> GameState
+update seconds = wallDeath . moveShip seconds
 
 wallCollision :: Position -> Radius -> Bool 
 wallCollision pos rad = xCollision pos rad || yCollision pos rad
@@ -100,9 +108,29 @@ yCollision (_, y) rad = (y + rad >= fromIntegral height/2) || (y - rad <= -fromI
 wallDeath :: GameState -> GameState
 wallDeath game = if wallCollision (shipLoc game) 20 then deathState else game
 
+handleKeys :: Event -> GameState -> GameState
+handleKeys (EventKey (Char 'w') Down _ _ ) game = game {shipVel = (xv, yv + 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 'a') Down _ _ ) game = game {shipVel = (xv - 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 's') Down _ _ ) game = game {shipVel = (xv, yv - 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 'd') Down _ _ ) game = game {shipVel = (xv + 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 'w') Up _ _ ) game = game {shipVel = (xv, yv - 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 'a') Up _ _ ) game = game {shipVel = (xv + 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 's') Up _ _ ) game = game {shipVel = (xv, yv + 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (Char 'd') Up _ _ ) game = game {shipVel = (xv - 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyUp) Down _ _ ) game = game {shipVel = (xv, yv + 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyLeft) Down _ _ ) game = game {shipVel = (xv - 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyDown) Down _ _ ) game = game {shipVel = (xv, yv - 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyRight) Down _ _ ) game = game {shipVel = (xv + 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyUp) Up _ _ ) game = game {shipVel = (xv, yv - 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyLeft) Up _ _ ) game = game {shipVel = (xv + 50, yv)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyDown) Up _ _ ) game = game {shipVel = (xv, yv + 50)} where (xv, yv) = shipVel game
+handleKeys (EventKey (SpecialKey KeyRight) Up _ _ ) game = game {shipVel = (xv - 50, yv)} where (xv, yv) = shipVel game
+handleKeys _ game = game 
+	
+
 
 main :: IO ()
-main = simulate window background fps initialState render update
+main = play window background fps initialState render handleKeys update
 
 
 
