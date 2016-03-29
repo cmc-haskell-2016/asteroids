afdd0b9e7e04c70cb824934058d0dce6bba5215e
gibsn <kirill-8@yandex.ru>
2016-03-29 17:10:38 +0300

    Added shiledOn to ship struct

--- src/Types.hs
+++ src/Types.hs
@@ -56,7 +56,8 @@ data Ship = Ship {
     shipVel :: ShipSpeed,
     shipAng :: Degree,
     rotation :: Degree,
-    shipAlive :: Bool
+    shipAlive :: Bool,
+    shieldOn :: Bool
 } deriving (Show, Eq)
 
 data Asteroid = Asteroid {
